a81556a1a89462ffc2beaadd833c3a3b5ac03d40
johannkrugell <johannkrugell@gmail.com>
2022-07-18 06:58:27 +1000

    ADD BASIC ARITHMETIC FUNCTIONS Add addition, subtraction, product and division functions to the calculator.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
+// declare variables to be 
+let term = ""
+let addends = ""
+let subtrahend = ""
 
+// addition function
+function addition (term, addends) {
+  sum =  term + addends;
+  return sum;
+}
+
+// subtracion function
+function subtraction (term, subtrahend) {
+  difference = term - subtrahend;
+  return difference;
+}
+
+// multiplication function
+function product (term, multiplicand) {
+  product = term * multiplicand;
+  return product;
+}
+
+// division function 
+function division (term, denominator) {
+  ratio = term / denominator;
+  return ratio;
+}
+
+// operator function, takes an operator and 2 numbers and calls the relevant function
+// on the numbers
 
