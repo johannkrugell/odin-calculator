3e91a76fe9c2f7b0e984ab8531ee0dc945c6749f
johannkrugell <johannkrugell@gmail.com>
2022-07-18 09:59:33 +1000

    ADD OPERATION FUNCTION Add an operation function that calls the relevant arithmetic function based on the operator provided. If the operator is "+" it will call the addition function.
    On branch operator_function
    Changes to be committed:
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 // declare variables to be 
+// term is used as the first number a user keys in
 let term = ""
+// addends is the number to be added to the the term
 let addends = ""
+// subtrahend is the number to be subtracted from the term
 let subtrahend = ""
+// multiplican is the number to be multiplied with the term
+let multiplican = ""
+// denominator is the number that divides the term
+let denominator = ""
+// operation variables
+let operator = "";
+let number1 = "";
+let number2 = "";
+
 
 // addition function
 function addition (term, addends) {
@@ -27,6 +39,17 @@ function division (term, denominator) {
   return ratio;
 }
 
-// operator function, takes an operator and 2 numbers and calls the relevant function
-// on the numbers
-
+// operator function, takes an operator and 2 numbers and calls the relevant 
+// function on the numbers
+function operation (operator, number1, number2) {
+ switch (operator){
+  case "+": return addition(number1,number2);
+  break;
+  case "-": return subtraction(number1,number2);
+  break;
+  case "*": return product(number1,number2);
+  break;
+  case "/": return division(number1,number2);
+  break;
+ }
+}
