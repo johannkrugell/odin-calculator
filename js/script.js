92d44c29d6aa030e5101e55a2ed04c50714a2180
johannkrugell <johannkrugell@gmail.com>
2022-07-21 06:11:13 +1000

    ADD EVENTLISTENERS Added event listeners to the calculator key pad. The value of each button clicked is stored in an array. The array is converted to text to be displayed on the calculatore screen
    On branch event-listeners
    Changes to be committed:
    modified:   index.html
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// string variable to store the value clicked on the calulator key pad
+let buttonValueClick = ""
+
+// eventlisteners added to all the buttons excludes equals and backspace
+const buttonValue = document.querySelectorAll(".value");
+for ( var i = 0; i < buttonValue.length; i++) { 
+    buttonValue[ i ].addEventListener( 'click', function() {
+      buttonValueClick = this.innerText;
+      console.log( this.innerText );
+      updateDisplay(buttonValueClick);
+   })
+}
+
+// display the values clicked on calculator display
+// declare an array to store the text values in
+let calculationArray = [];
+
+// push each value clicked onto the array and convert to string to be displayed
+// on the calculator
+function updateDisplay(buttonValueClick) {
+  calculationArray.push(buttonValueClick);
+  let calculatorDisplay = calculationArray.toString().replace(/,/g,"");
+  document.getElementsByClassName("calculator-display")[0].innerText = calculatorDisplay
+  console.log(calculationArray);
+  console.log(calculatorDisplay);
+}
+
+
 // declare variables to be 
 // term is used as the first number a user keys in
 let term = ""
@@ -53,3 +81,4 @@ function operation (operator, number1, number2) {
   break;
  }
 }
+
