bb9811f6067cabec2b517c6f59262a12864ec86b
johannkrugell <johannkrugell@gmail.com>
2022-07-28 05:39:11 +1000

    WORKING CALCULATOR Added event listeners for the buttons, operators and equals operator. The basic workings are as follows: 1. The calculator listens for the click of a number 2. The number value is stored in updateValueClicked() 3. The calculator listens for the click of an operator 4. The operator value is stored in updateOperatorClicked() 5. The updateOperatorClicked() function checks if the operator is already clicked once. If it is, it will store the value clicked in value1. 6. The calculator listens for another operator or the equals sign. If another operator has been clicked the value is stored in value2. 7. Now two values and an operator has been stored the calculation is called. Based on the first operator in operator array the calculation is performed. 8. The result is stored in interimresult. value1 and value2 is cleared. The first value in the operator array is removed 9. The calculator listens for another value button, because there is an interimresult the value is store in value2. The steps above are repeated.
    On branch working-calculator
    Changes to be committed:
    modified:   index.html
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,84 +1,246 @@
+// declare variables  
+// value1 and value2 are used to store the button values before the user 
+// press equals
+let value1 = "";
+let value2 = "";
+// interimresult is used to store the result of the calculation performed on 
+// value1 and value2 before the user press equals. For example 1 + 1 - 1 the
+// addition of 1+1 is stored in the interimResult before 1 is deducted
+let interimResult = "";
 // string variable to store the value clicked on the calulator key pad
-let buttonValueClick = ""
+let buttonValueClick = "";
+// set default display value of the calculator is zero
+document.getElementsByClassName("calculator-display")[0].innerText = 0;
 
-// eventlisteners added to all the buttons excludes equals and backspace
+// eventlisteners added to the value buttons
+// If there is an interim result and not value1/2 then the user has clicked = 
+// in previous step and the value clicked now should be assigned to value1/2. 
+// In all other cases the user is just keying in the number and only the display
+// is updated
 const buttonValue = document.querySelectorAll(".value");
-for ( var i = 0; i < buttonValue.length; i++) { 
-    buttonValue[ i ].addEventListener( 'click', function() {
+for (var i = 0; i < buttonValue.length; i++) { 
+  buttonValue[i].addEventListener( 'click', function() {
+    if (value1 === "" && value2 === "" && interimResult !== "") {
       buttonValueClick = this.innerText;
-      console.log( this.innerText );
       updateDisplay(buttonValueClick);
-   })
+      checkOperator(buttonValueClick);
+    }
+    else {
+      buttonValueClick = this.innerText;
+      updateDisplay(buttonValueClick); // update the value display on calculator  
+    }
+  })
 }
 
-// display the values clicked on calculator display
-// declare an array to store the text values in
-let calculationArray = [];
+// eventlistener for the operator button
+// entering the operator determines to which variable the button values are 
+// assigned. If the operator array length is 1 then the buttonvalues should be
+// assigned to value1 and the running display updated. If the operator length
+// is more than one then the values should be stored in value2 and a calculation 
+// performed
+const operatorValue = document.querySelectorAll(".operator");
+const operatorValueClick = [];
 
-// push each value clicked onto the array and convert to string to be displayed
-// on the calculator
+for (var i = 0; i < operatorValue.length; i++) { 
+  operatorValue[i].addEventListener( 'click', function() {
+      operatorValueClick.push(this.innerText); // add operator value to array
+    if (operatorValueClick.length > 1) {
+      checkOperator(operatorValueClick); // store the values in value2
+      calculation(value1,value2); // performs calculation
+      return operatorValueClick;
+    } else {
+      checkOperator(operatorValueClick); // store the values in value1
+      updateDisplayCalculation(operatorValueClick); // update calculator display
+      return operatorValueClick;
+    }       
+  })
+}
+
+// eventlistener for the equals operator
+const equal = document.querySelectorAll(".equals");
+
+for ( var i = 0; i < equal.length; i++) { 
+  equal[ i ].addEventListener( 'click', function() {
+    if (buttonValueClick !== value2.toString()) {
+      checkOperator(buttonValueClick);
+    }
+    calculation(value1,value2); // perform calculation
+    buttonValueClick = "";
+  })
+}
+
+// eventlistener for the clear all button
+const allClear = document.querySelectorAll(".clear");
+// clears all the variable values and set the calculator display to 0
+for ( i = 0; i < allClear.length; i++) { 
+  allClear[ i ].addEventListener( 'click', function() {
+    value1 = "";
+    value2 = "";
+    interimResult = "";
+    buttonValueClick = "";
+    calculationArray.splice(0,calculationArray.length);
+    operatorValueClick.splice(0,operatorValueClick.length);
+    document.getElementsByClassName("calculator-display")[0].innerText = "0"
+  })
+}
+
+// eventlister for the backspace button
+const clearLast = document.querySelectorAll(".backspace");
+
+for ( i = 0; i < clearLast.length; i++ ) {
+  clearLast[i].addEventListener( 'click', function() {
+    if ( operatorValueClick.length === 1 && value2 === "") {
+      operatorValueClick.pop(); // if an operator has been clicked the backspace
+                                // removes the operator
+      document.getElementsByClassName("calculator-display")[0].innerText = 
+      value1.toString().replace(/,/g,"");
+    } else {
+      calculationArray.pop(); // if no operator has been click, remove the last
+                              // button value from the array
+      document.getElementsByClassName("calculator-display")[0].innerText =
+      calculationArray.toString().replace(/,/g,"");
+    }  
+  })
+}
+
+// declare an array to store number values the user have clicked
+const calculationArray = [];
+
+// push value clicked onto array that is displaye on the calculator
 function updateDisplay(buttonValueClick) {
-  calculationArray.push(buttonValueClick);
+  calculationArray.push(buttonValueClick); // push button value clicked on array
   let calculatorDisplay = calculationArray.toString().replace(/,/g,"");
-  document.getElementsByClassName("calculator-display")[0].innerText = calculatorDisplay
-  console.log(calculationArray);
-  console.log(calculatorDisplay);
+  document.getElementsByClassName("calculator-display")[0].innerText 
+  = calculatorDisplay;
+  if (value1 !== "" && value2 == "") {
+    checkOperator(buttonValueClick);
+  }
+} 
+
+// function to store number value click in either value1 or value2
+// after storing the value the array with numbers is cleared to store new values
+function checkOperator(){
+  if (operatorValueClick.length === 1 && value1 === "" && interimResult === "") {
+    value1 = parseInt(calculationArray.toString().replace(/,/g,""));
+    calculationArray.splice(0,calculationArray.length);
+  } else if (operatorValueClick.length === 1 && value1 === "" && value2 === "" &&
+    interimResult !== "" && buttonValueClick === "" ) {
+    // do nothing there is an interim result but only a new operator is clicked
+  } else if (operatorValueClick.length > 1 && value1 !== "" && value2 !== "" &&
+    interimResult === "" && buttonValueClick !== "" ) {
+    // do nothing there are values assigned and should go to calculation
+  } else {
+    value2 = parseInt(calculationArray.toString().replace(/,/g,""));
+    calculationArray.splice(0,calculationArray.length);
+  }  
 }
 
+// update display during calculations
+function updateDisplayCalculation() {
+  if ( operatorValueClick.length > 1 ) { 
+    // if operator has been clicked twice display the interimResult
+    document.getElementsByClassName("calculator-display")[0].innerText 
+    = interimResult;
+    operatorValueClick.shift();
+  } else if (operatorValueClick.length === 1 && interimResult === "") {
+    // operator clicked once, display the number and the operator
+    let valueOperator = value1.toString().concat(operatorValueClick.toString());
+    document.getElementsByClassName("calculator-display")[0].innerText 
+    = valueOperator;
+  } else if (operatorValueClick.length === 1 && interimResult !== "" 
+             && value1 === "" && value2 === "") {
+    let valueOperator = interimResult.toString().concat(operatorValueClick.toString());
+    document.getElementsByClassName("calculator-display")[0].innerText 
+    = valueOperator;        
+  } 
+  else {
+    // in all other case calculaton has been performed, display result and 
+    // clear variables
+    document.getElementsByClassName("calculator-display")[0].innerText 
+    = interimResult;
+    value1 = "";
+    value2 = "";
+    operatorValueClick.shift();
+    }
+  }
 
-// declare variables to be 
-// term is used as the first number a user keys in
-let term = ""
-// addends is the number to be added to the the term
-let addends = ""
-// subtrahend is the number to be subtracted from the term
-let subtrahend = ""
-// multiplican is the number to be multiplied with the term
-let multiplican = ""
-// denominator is the number that divides the term
-let denominator = ""
-// operation variables
-let operator = "";
-let number1 = "";
-let number2 = "";
-
+// checks the operator in the array, based on operator value performs appropriate
+// calculation
+  function calculation() {
+  switch(operatorValueClick[0]) {
+    case "+" :
+      if (interimResult === "") {
+        addition(value1,value2);  
+      } else {
+        addition(interimResult,value2);
+      }
+    break;
+    case "-":
+      if (interimResult === "") {
+        subtraction(value1,value2);  
+      } else {
+        subtraction(interimResult,value2);
+      } 
+    break;
+    case "*":
+      if (interimResult === "") {
+        product(value1,value2);  
+      } else {
+        product(interimResult,value2);
+      } 
+    break;
+    case "/":
+      if (value2 === 0 ) {
+        document.getElementsByClassName("calculator-display")[0].innerText = 
+        "Dont be silly";
+      } else if (interimResult === "") {
+        division(value1,value2);  
+      } else {
+        division(interimResult,value2);
+      } 
+    break;
+    case "%":
+      if (interimResult === "") {
+        modulo(value1,value2);  
+      } else {
+        modulo(interimResult,value2);
+      } 
+    break;
+    }
+}
 
 // addition function
+let sum = []
 function addition (term, addends) {
-  sum =  term + addends;
-  return sum;
+  interimResult =  term + addends;
+  updateDisplayCalculation();
+  return interimResult;
 }
 
-// subtracion function
+// subtration function
 function subtraction (term, subtrahend) {
-  difference = term - subtrahend;
-  return difference;
+  interimResult = term - subtrahend;
+  updateDisplayCalculation();
+  return interimResult;
 }
 
 // multiplication function
 function product (term, multiplicand) {
-  product = term * multiplicand;
-  return product;
+  interimResult = term * multiplicand;
+  updateDisplayCalculation();
+  return interimResult;
 }
 
 // division function 
 function division (term, denominator) {
-  ratio = term / denominator;
-  return ratio;
+  interimResult = term / denominator;
+  updateDisplayCalculation();
+  return interimResult;
 }
 
-// operator function, takes an operator and 2 numbers and calls the relevant 
-// function on the numbers
-function operation (operator, number1, number2) {
- switch (operator){
-  case "+": return addition(number1,number2);
-  break;
-  case "-": return subtraction(number1,number2);
-  break;
-  case "*": return product(number1,number2);
-  break;
-  case "/": return division(number1,number2);
-  break;
- }
+// modulo function 
+function modulo (term, denominator) {
+  interimResult = term % denominator;
+  updateDisplayCalculation();
+  return interimResult;
 }
-
