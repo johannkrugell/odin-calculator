9b0f53245e80f6b7e0c101bce2a0fc5d5f81879f
johannkrugell <johannkrugell@gmail.com>
2022-08-15 05:52:19 +1000

    DISCORD FEEDBACK Users on discord identified three errors. 1. If if spam type the calculator stops working. 2. if a number has more than 10 digits it does wrap in the display. 3. Pressing equals and continuing with the calculation stops the calculator for example 19*3=57+5 would not work
    These errors have been fixed and the calculator is now working as
    intended. The layout of the calculator has also been changed.
    
    On branch feedback-discord
    Changes to be committed:
    modified:   css/stylesheet.css
    modified:   index.html
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,71 @@
 // declare variables  
-let value1 = ""; // stores numberValues based on checkOperator() assignment
+let value1 = ""; // stores numberValues based on storeValue() assignment
 let value2 = "";
-let interimResult = ""; // stores calculation result before equals is clicked
 let numberValue = ""; // number value clicked on the calulator key pad
+const interimResult = []; // stores the interim calculation result
+const operatorList = ["+","-","*","/","%"]; // to remove operators when backspace
+const calculationDisplay = [];
 
 // set default display value of the calculator is zero
-document.getElementsByClassName("calculator-display")[0].innerText = 0;
+document.getElementsByClassName("calculator-display")[0].innerText = "";
 
 // Event Listeners 
 // Keyboard support 
 document.addEventListener( 'keydown', function(event) {
-  if (event.key >= 0 && event.key <= 9) {
+  if (parseInt(event.key) >= 0 && parseInt(event.key) <= 9) {
     numbersClicked(event.key);
-  }
-  else if (event.key === "/" || event.key === "*" || event.key === "-"
-           || event.key === "+" || event.key === "%") {
+  } else if (operatorList.indexOf(event.key) !== -1) {
+    event.preventDefault();
     operatorClicked(event.key);
-  }
-  else if (event.key === "Enter"){
-    equalsClicked(event.key);
-  }   
-  else if (event.key === "Backspace"){
+  } else if (event.key === "Enter"){
+    operatorClicked(event.key);
+  } else if (event.key === "Backspace" && numberValue === ""){
+    // do nothing
+  } else if (event.code === "Space"){
+    // do nothing 
+  } else if (event.key === "Backspace"){
     backspace();
-  }
-  else if (event.key === "c" || event.key === "C") {
+  } else if (event.key === "c" || event.key === "C") {
     clearAll();
+  } else if (event.key === ".") {
+    decimalClicked(event.key);
   }
 })
 
-// Numbers UI
+// Numbers UI event
 const numbers = document.querySelectorAll(".number");
 for (var i = 0; i < numbers.length; i++) { 
   numbers[i].addEventListener( 'click', function()
-  {numbersClicked(this.innerText)})
+  { numbersClicked(this.innerText) })
 }
 
-function numbersClicked (value) {
-  if (value1 === "" && value2 === "" && interimResult !== "") {
-    numberValue = value;
-    updateDisplay(numberValue); // assigns numberValue to value1 / value2
-    checkOperator(numberValue); // update the display on calculator
+function numbersClicked(value) {
+  numberValue = value
+  pushArray(calculationArray, numberValue);
+  pushArray(calculationDisplay, numberValue);
+  refreshCalculationDisplay();
+  if (interimResult.length <= 1) {
+    calculation(value1,parseFloat(toString(calculationArray)));
+  } else if (interimResult.length > 1) {
+    calculation(interimResult[interimResult.length-1],
+    parseFloat(toString(calculationArray)));
+  } else if (interimResult.length === 1) {
+    calculation(interimResult,parseFloat(toString(calculationArray)));
   }
-  else {
-    numberValue = value
-    updateDisplay(numberValue); // update the display on calculator  
-  }
+}
+
+// Decimal UI
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener('click', function()
+  { decimalClicked(this.innerText) })
+
+function decimalClicked(value) {
+  if (decimal.disabled === false) {
+    pushArray(calculationArray,value);
+    pushArray(calculationDisplay,value)
+    refreshCalculationDisplay(); 
+    decimal.setAttribute("disabled", '');
+  } 
 }
 
 // Operator UI
@@ -53,35 +74,41 @@ const operatorValue = [];
 
 for (var i = 0; i < operators.length; i++) { 
   operators[i].addEventListener( 'click', function() 
-  {operatorClicked(this.innerText)})
+  { operatorClicked(this.innerText) })
 }
 
 function operatorClicked(value) {
-  operatorValue.push(value); // add operator value to array
-    if (operatorValue.length > 1) {
-      checkOperator(operatorValue); // store the values in value2
-      calculation(value1,value2); // performs calculation
-      return operatorValue;
-    } else {
-      checkOperator(operatorValue); // store the values in value1
-      updateDisplayCalculation(operatorValue); // update calculator display
-      return operatorValue;
-    }
+  pushArray(operatorValue,value);
+  document.querySelector(".decimal").removeAttribute("disabled"); // enable decimal
+  if (operatorValue[operatorValue.length-1] === "Enter" || 
+      operatorValue[operatorValue.length-1] === "=") {
+    styleCalculationDisplay();
+    clearArray(calculationDisplay, 0, calculationDisplay.length);
+    clearArray(operatorValue, 0, operatorValue.length);
+    clearArray(calculationArray, 0, calculationArray.length);
+    pushArray(interimResult, interimResult[interimResult.length-1]);
+  } else if (operatorValue.length >= 1 && value1 !== "") {
+    styleDisplay();
+    clearArray(operatorValue, 0, operatorValue.length-1);
+    clearArray(calculationArray, 0, calculationArray.length);
+    pushArray(calculationDisplay, value);
+    refreshCalculationDisplay();
+  } else {
+    storeValue(operatorValue); 
+    pushArray(calculationDisplay, value);
+    refreshCalculationDisplay();
+  }
 }
 
 // Equals UI
 const equal = document.querySelectorAll(".equals");
 
 for ( var i = 0; i < equal.length; i++) { 
-  equal[ i ].addEventListener( 'click', function() {equalsClicked()})
+  equal[ i ].addEventListener( 'click', function() {equalsClicked(this.innerText)})
 }
 
-function equalsClicked() {
-  if (numberValue !== value2.toString()) {
-    checkOperator(numberValue);
-  }
-    calculation(value1,value2); // perform calculation
-    numberValue = "";
+function equalsClicked(value) {
+  operatorClicked(value);
 }
 
 // Clear all UI
@@ -94,11 +121,12 @@ for ( i = 0; i < allClear.length; i++) {
 function clearAll() {
   value1 = "";
   value2 = "";
-  interimResult = "";
   numberValue = "";
-  calculationArray.splice(0,calculationArray.length);
-  operatorValue.splice(0,operatorValue.length);
-  document.getElementsByClassName("calculator-display")[0].innerText = "0"
+  clearArray(calculationArray, 0, calculationArray.length);
+  clearArray(calculationDisplay, 0, calculationDisplay.length);
+  clearArray(interimResult, 0, interimResult.length);
+  clearArray(operatorValue, 0, operatorValue.length);
+  blankCalculationDisplay();
 }
 
 // Backspace UI
@@ -109,16 +137,31 @@ for ( i = 0; i < clearLast.length; i++ ) {
 }
 
 function backspace() {
-  if ( operatorValue.length === 1 && value2 === "") {
-    operatorValue.pop(); // if an operator has been clicked the backspace
-                         // removes the operator
-    document.getElementsByClassName("calculator-display")[0].innerText = 
-    value1.toString().replace(/,/g,"");
+  if (operatorList.indexOf(calculationDisplay[calculationDisplay.length-1]) !== -1) {
+    popArray(operatorValue);
+    popArray(calculationArray);
+    popArray(calculationDisplay);                
+    refreshCalculationDisplay(); 
+    if (interimResult.length === 0){
+      updateDisplay("");
+    } else {
+      updateDisplay(interimResult[interimResult.length-1]);
+    } 
+  } else if (interimResult.length === 0 || calculationArray[calculationArray.length-1] === ".") {
+    popArray(calculationArray);
+    popArray(calculationDisplay);
+    refreshCalculationDisplay();
+    document.querySelector(".decimal").removeAttribute("disabled");
   } else {
-    calculationArray.pop(); // if no operator has been click, remove the last
-                            // button value from the array
-    document.getElementsByClassName("calculator-display")[0].innerText =
-    calculationArray.toString().replace(/,/g,"");
+    popArray(calculationArray); 
+    popArray(calculationDisplay);
+    popArray(interimResult);
+    refreshCalculationDisplay();
+    if (interimResult.length === 0){
+      updateDisplay("");
+    } else {
+      updateDisplay(interimResult[interimResult.length-1]);
+    }  
   }
 }
 
@@ -126,139 +169,122 @@ function backspace() {
 const calculationArray = [];
 
 // push value clicked onto array that is displayed on the calculator
-function updateDisplay(numberValue) {
-  calculationArray.push(numberValue); 
-  let calculatorDisplay = calculationArray.toString().replace(/,/g,"");
-  document.getElementsByClassName("calculator-display")[0].innerText 
-  = calculatorDisplay;
-  if (value1 !== "" && value2 == "") {
-    checkOperator(numberValue);
-  }
+function updateDisplay(value) {
+  document.getElementsByClassName("calculator-display")[0].innerText = value;
+  styleDisplay();
 } 
 
 // Store number value click in either value1 or value2
-function checkOperator(){
-  if (operatorValue.length === 1 && value1 === "" && interimResult === "") {
-    value1 = parseInt(calculationArray.toString().replace(/,/g,""));
-    calculationArray.splice(0,calculationArray.length);
-  } else if (operatorValue.length === 1 && value1 === "" && value2 === "" &&
-    interimResult !== "" && numberValue === "" ) {
-    // do nothing there is an interim result but only a new operator is clicked
-  } else if (operatorValue.length > 1 && value1 !== "" && value2 !== "" &&
-    interimResult === "" && numberValue !== "" ) {
-    // do nothing there are values assigned and should go to calculation
+function storeValue() {
+  if (operatorValue.length === 1 ) {
+    value1 = parseFloat(calculationArray.toString().replace(/,/g,""));
+    clearArray(calculationArray, 0, calculationArray.length);
   } else {
     value2 = parseInt(calculationArray.toString().replace(/,/g,""));
-    calculationArray.splice(0,calculationArray.length);
-  }  
+    clearArray(calculationArray, 0, calculationArray.length);
+  }
 }
 
 // update display during calculations
-function updateDisplayCalculation() {
-  if ( operatorValue.length > 1 ) { 
-    // if operator has been clicked twice display the interimResult
-    document.getElementsByClassName("calculator-display")[0].innerText 
-    = interimResult;
-    operatorValue.shift();
-  } else if (operatorValue.length === 1 && interimResult === "") {
-    // operator clicked once, display the number and the operator
-    let valueOperator = value1.toString().concat(operatorValue.toString());
-    document.getElementsByClassName("calculator-display")[0].innerText 
-    = valueOperator;
-  } else if (operatorValue.length === 1 && interimResult !== "" 
-             && value1 === "" && value2 === "") {
-    let valueOperator = interimResult.toString().concat(operatorValue.toString());
-    document.getElementsByClassName("calculator-display")[0].innerText 
-    = valueOperator;        
-  } 
-  else {
-    // in all other case calculaton has been performed, display result and 
-    // clear variables
-    document.getElementsByClassName("calculator-display")[0].innerText 
-    = interimResult;
-    value1 = "";
-    value2 = "";
-    operatorValue.shift();
-    }
+function refreshCalculationDisplay(value) {
+  let calculationDisplayString = calculationDisplay.toString().replace(/,/g,"");
+  document.getElementsByClassName("calculation-display")[0].innerText 
+    = calculationDisplayString
   }
 
-// checks the operator in the array, based on operator value performs appropriate
-// calculation
-  function calculation() {
+// check operator, perform calculation
+function calculation(anyValue1, anyValue2) {
   switch(operatorValue[0]) {
     case "+" :
-      if (interimResult === "") {
-        addition(value1,value2);  
-      } else {
-        addition(interimResult,value2);
-      }
+      addition(anyValue1, anyValue2);  
+      updateDisplay(interimResult[interimResult.length-1]);
     break;
     case "-":
-      if (interimResult === "") {
-        subtraction(value1,value2);  
-      } else {
-        subtraction(interimResult,value2);
-      } 
+      subtraction(anyValue1, anyValue2);  
+      updateDisplay(interimResult[interimResult.length-1]);
     break;
     case "*":
-      if (interimResult === "") {
-        product(value1,value2);  
-      } else {
-        product(interimResult,value2);
-      } 
+      product(anyValue1, anyValue2);
+      updateDisplay(interimResult[interimResult.length-1]);  
     break;
     case "/":
-      if (value2 === 0 ) {
+      if (anyValue2 === 0 ) {
         document.getElementsByClassName("calculator-display")[0].innerText = 
         "Dont be silly";
-      } else if (interimResult === "") {
-        division(value1,value2);  
       } else {
-        division(interimResult,value2);
+        division(anyValue1, anyValue2);
+        updateDisplay(interimResult[interimResult.length-1]);  
       } 
     break;
     case "%":
-      if (interimResult === "") {
-        modulo(value1,value2);  
-      } else {
-        modulo(interimResult,value2);
-      } 
+      modulo(anyValue1, anyValue2);  
+      updateDisplay(interimResult[interimResult.length-1]);
     break;
-    }
+  }
 }
 
 // addition function
 let sum = []
 function addition (term, addends) {
-  interimResult =  term + addends;
-  updateDisplayCalculation();
-  return interimResult;
+  interimResult.push(term + addends).toFixed(2);
 }
 
 // subtration function
 function subtraction (term, subtrahend) {
-  interimResult = term - subtrahend;
-  updateDisplayCalculation();
-  return interimResult;
+  interimResult.push(term - subtrahend);
 }
 
 // multiplication function
 function product (term, multiplicand) {
-  interimResult = term * multiplicand;
-  updateDisplayCalculation();
-  return interimResult;
+  interimResult.push(term * multiplicand);
 }
 
 // division function 
 function division (term, denominator) {
-  interimResult = term / denominator;
-  updateDisplayCalculation();
-  return interimResult;
+  interimResult.push(term / denominator);
 }
 
 // modulo function 
 function modulo (term, denominator) {
-  interimResult = term % denominator;
-  updateDisplayCalculation();
-  return interimResult;
+  interimResult.push(term % denominator);
+}
+
+// Helper Functions
+// Parse calculation array to string
+function toString(value) {
+  return value.toString().replace(/,/g,"");
+}
+
+// Update calculator display style
+function styleDisplay() {
+  document.getElementsByClassName("calculator-display")[0].style.fontSize = "30px";
+  document.getElementsByClassName("calculator-display")[0].style.color = "gray";
+}
+
+// Update calculation display
+function styleCalculationDisplay() {
+  document.getElementsByClassName("calculation-display")[0].innerText = "";
+  document.getElementsByClassName("calculator-display")[0].style.color = "black";
+  document.getElementsByClassName("calculator-display")[0].style.fontSize = "40px";
+}
+
+// add value/operator/interim result to array
+function pushArray(array, value) {
+  return array.push(value);
+}
+
+// remove value/operator/interim result from array
+function popArray(array) {
+  return array.pop();
+}
+
+// clear all items in array
+function clearArray(array, position, items) {
+  return array.splice(position, items);
+}
+
+// set calculation display to blank
+function blankCalculationDisplay() {
+  document.getElementsByClassName("calculator-display")[0].innerText = "";
+  document.getElementsByClassName("calculation-display")[0].innerText = "";
 }
